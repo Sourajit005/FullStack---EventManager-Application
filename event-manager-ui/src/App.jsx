adb6b2fd8facac97c2feb0227e76d54610f6ae81
Sourajit Sahoo <srrt2003@gmail.com>
2025-11-02 12:51:31 +0530

    Fix component import casing for Linux

--- event-manager-ui/src/App.jsx
+++ event-manager-ui/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import OrganizerDashboard from './pages/OrganizerDashboard';
 import ProtectedRoute from './components/ProtectedRoute';
 import EventDetailsPage from './pages/EventDetailsPage';
